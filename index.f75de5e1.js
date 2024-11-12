7c8eb6af571e2770fd4e3c86d0a76a69846794b2
Max Hrebinets <brainisbro@Gmail.com>
2024-11-12 16:39:12 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const newBlock = document.createElement("div");
+    newBlock.classList.add("notification", type);
+    newBlock.style.top = `${posTop}px`;
+    newBlock.style.right = `${posRight}px`;
+    const blockTitle = document.createElement("h2");
+    blockTitle.classList.add("title");
+    blockTitle.textContent = title;
+    newBlock.append(blockTitle);
+    const blockDescription = document.createElement("p");
+    blockDescription.textContent = description;
+    newBlock.append(blockDescription);
+    document.body.append(newBlock);
+    setTimeout(()=>{
+        newBlock.remove();
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
